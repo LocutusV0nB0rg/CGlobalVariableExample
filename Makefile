82cd48ed1eecbea2ff6443d120712dbc4f013d83
LocutusV0nB0rg <postfuererich2@gmail.com>
2023-05-04 14:50:34 +0200

    Added second increment function

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 all:
 	gcc -Wall -Wextra -c inventory.c -o inventory.o
-	gcc -Wall -Wextra inventory.o main.c -o main
+	gcc -Wall -Wextra -c utils.c -o utils.o
+	gcc -Wall -Wextra inventory.o utils.o main.c -o main
--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include "inventory.h"
+#include "utils.h"
+
 #include <stdio.h>
 
 int main() {
-    printf("%d", combat_stat);
+    printf("%d\n", combat_stat);
     increment();
-    printf("%d", combat_stat);
-
-
-
+    printf("%d\n", combat_stat);
+    utilsinc();
+    printf("%d\n", combat_stat);
+    increment();
+    printf("%d\n", combat_stat);
 }
